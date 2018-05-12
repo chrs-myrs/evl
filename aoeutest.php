d2ef537224e8b791af24b6e68a83859c2ab5a316
Chris Myers <chris@myrs.uk>
2018-05-12 18:25:53 +0100

    build test

--- aoeutest.php
+++ aoeutest.php
@@ -1,2 +1,2 @@
 <?php
-echo file_get_contents("http://fulltime-league.thefa.com/ProcessPublicSelect.do?psSelectedSeason=200684883&psSelectedDivision=856494335&psSelectedCompetition=0&psSelectedLeague=2025888");
+echo "hi";
